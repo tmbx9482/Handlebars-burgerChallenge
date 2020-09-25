423bbd8c6f9f173008874b987fe7ab722da52f6d
Troy Berentsen <troyberentsen94@gmail.com>
2020-09-25 14:40:00 -0400

    Server is connecting from PORT 4000

--- db/Schema.sql
+++ db/Schema.sql
@@ -3,25 +3,28 @@
 -- * Letting the user know to drop database if not created **
 DROP DATABASE IF EXISTS burgers_db;
 
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 --    * Create the `burgers_db`.
 --    * Switch to or use the `burgers_db`.
 --    * Create a `burgers` table with these fields:
---      * **id**: an auto incrementing int that serves as the primary key.
---      * **burger_name**: a string.
---      * **devoured**: a boolean.
+--    * **id**: an auto incrementing int that serves as the primary key.
+--    * **burger_name**: a string.
+--    * **devoured**: a boolean.
 
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(100) NOT NULL,
+	id int NOT NULL
+	AUTO_INCREMENT,
+	burger_name varchar
+	(100) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+	(id)
 );
 
-
-SELECT * FROM burgers
+	-- Able to choose which burger you want
+	SELECT *
+	FROM burgers
---
+++ server.js
@@ -0,0 +1,32 @@
+// For more reference on how this page became about checkout link:https://youtu.be/msvdn95x9OM
+// Require Express
+let express = require("express");
+// Local Host connection
+let PORT = process.env.PORT || 4000;
+// defining express name
+let app = express();
+
+//static content from the "public" directory is enable for app.
+// app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+// Parse application => JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Hello Handlebars!
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Having the routes import the server.js
+let routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+// Start server + begin listening to client requests = at PORT 4000!
+app.listen(PORT, function() {
+  // console.log to test to connection
+  console.log("Server listening on: http://localhost:" + PORT);
+});
